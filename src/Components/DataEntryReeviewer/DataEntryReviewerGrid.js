96586dd760ae6c4789597c63a411d8f9aa2ee7d3
Srinivasan Chandrasekaran <srinivasan_chandrasekaran@trimble.com>
2023-06-18 21:02:54 +0530

    minor changes

---
+++ src/Components/DataEntryReeviewer/DataEntryReviewerGrid.js
@@ -0,0 +1,7 @@
+import react from "react";
+import ReviewGrid from "../FormReviewer/ReviewGrid"
+
+const DataEntryReviewerGrid = () => {
+    return <ReviewGrid />
+}
+export default DataEntryReviewerGrid;
