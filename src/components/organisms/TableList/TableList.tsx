8975f07b7cb38f5df57efc48396de231ed3c8be5
Srinivasan Chandrasekaran <srinivasan_chandrasekaran@trimble.com>
2023-07-16 12:44:57 +0530

    refactoring - iniital commit

---
+++ src/components/organisms/TableList/TableList.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Table } from "@mui/material";
+import { DataGrid, GridMoreVertIcon } from "@mui/x-data-grid";
+export interface ITableListProps {
+  columns: [];
+  rowClick: any;
+  data: [];
+}
+function TableList(props: ITableListProps) {
+  const { columns, rowClick, data } = { ...props };
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        onRowClick={rowClick}
+      />
+    </div>
+  );
+}
+export default TableList;
